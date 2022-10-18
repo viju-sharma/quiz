899944bc390d34bbbefedee7c76fd6fcc512e1e9
viju-sharma <vijendersharma47@gmail.com>
2022-10-19 01:46:09 +0530

    minor

--- client/src/Components/Admin/AdminDashboard.jsx
+++ client/src/Components/Admin/AdminDashboard.jsx
@@ -27,20 +27,21 @@ const AdminDashboard = () => {
         <div className="container mx-auto">
           <h1>All Quizs</h1>
           <hr></hr>
-          {quizs.map((quiz, index) => (
-            <div
-              key={index}
-              className="container flex gap-28 mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10 "
-            >
-              <p className="text-lg">{quiz.subject}</p>
-              <Link to={`test/${quiz._id}`}>
-                <button className="ext-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-                  {" "}
-                  Start Test
-                </button>
-              </Link>
-            </div>
-          ))}
+          {quizs.length > 0 &&
+            quizs.map((quiz, index) => (
+              <div
+                key={index}
+                className="container flex gap-28 mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10 "
+              >
+                <p className="text-lg">{quiz.subject}</p>
+                <Link to={`test/${quiz._id}`}>
+                  <button className="ext-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                    {" "}
+                    Start Test
+                  </button>
+                </Link>
+              </div>
+            ))}
         </div>
       </div>
     </Fragment>
